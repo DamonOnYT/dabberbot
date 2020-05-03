2c06253806929e992e762adccd85bc25529b4cbb
DamonOnYT <damon@ddidevelopment.com>
2020-05-03 16:24:18 +1000

    Update dabberbot.js

--- dabberbot.js
+++ dabberbot.js
@@ -107,7 +107,6 @@ fs.readdir("./commands/", (err, files) => {
     let props = require(`./commands/${file}`);
     let commandName = file.split(".")[0];
     console.log(`Attempting to load command ${commandName}`);
-    client.commands.set(commandName, props);
   });
 });
 
