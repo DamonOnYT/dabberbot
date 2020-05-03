a4b4e3dd0e4d5d363bf3594e0990a526e68460f6
DamonOnYT <damon@ddidevelopment.com>
2020-05-03 16:37:59 +1000

    Update dabberbot.js

--- dabberbot.js
+++ dabberbot.js
@@ -89,14 +89,12 @@ function clean(text) {
 }
 
 
-// This loop reads the /events/ folder and attaches each event file to the appropriate event.
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
-    let eventFunction = require(`./events/${file}`);
+    const event = require(`./events/${file}`);
     let eventName = file.split(".")[0];
-    // super-secret recipe to call events with all their proper arguments *after* the `client` var.
-    client.on(eventName, (...args) => eventFunction.run(client, ...args));
+    client.on(eventName, event.bind(null, client));
   });
 });
 
