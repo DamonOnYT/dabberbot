804f71b19d196559b7a88ac25372df5ce3e7fc4c
DamonOnYT <damon@ddidevelopment.com>
2020-05-03 16:23:29 +1000

    Update dabberbot.js

--- dabberbot.js
+++ dabberbot.js
@@ -100,6 +100,17 @@ fs.readdir("./events/", (err, files) => {
   });
 });
 
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    client.commands.set(commandName, props);
+  });
+});
+
 client.on("message", async (message) => {
  if(!message.guild || message.author.bot) return;
 // We can use ensure() to actually grab the default value for settings,
