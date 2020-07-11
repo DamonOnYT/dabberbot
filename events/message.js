b35560a4a7b0562812bd81783009a4e90b7092d8
DamonOnYT <DemonProductions@mail.com>
2020-07-11 22:49:59 +1000

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,9 +1,9 @@
 module.exports = (client, message) => {
-  // Ignore all bots
   if (message.author.bot) return;
+  const guildConf = client.settings.ensure(message.guild.id, defaultSettings);
 
   // Ignore messages not starting with the prefix (in config.json)
-  if (message.content.indexOf(client.config.prefix) !== 0) return;
+  if(message.content.indexOf(guildConf.prefix) !== 0) return;
 
   // Our standard argument/command name definition.
   const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
